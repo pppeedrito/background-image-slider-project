7d237b73accf63301a9d27440c0954681f53c91e
pedro nin <pedro1996616@gmail.com>
2020-12-22 11:23:41 +0100

    Acabado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+  const btnleft = document.querySelector(".btn-left");
+  const btnrigt = document.querySelector(".btn-right");
+  var counter = 0;
+  var change = document.querySelector(".img-container");
   const pictures = [
     "contBcg-0",
     "contBcg-1",
@@ -5,3 +9,21 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+function nextPic(e){
+  if(e.target.id == "btnright"){
+    counter += 1;
+    if (counter > pictures.length -1) {
+      counter = 0;
+    }
+  }else{
+   if (counter<= 0){
+     counter = 4;
+   }   else{
+   counter -= 1;
+  }
+  }
+  change.style.backgroundImage = "url("+"../img/" + pictures[counter] + ".jpeg)";
+}
+btnleft.addEventListener('click',nextPic);
+btnrigt.addEventListener('click',nextPic);
